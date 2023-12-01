a3bc759e6c6be7b234fa716c3946428f5ac5f13d
Rafael Sant Anna <rafaeel.16@gmail.com>
2023-12-01 17:50:07 -0300

    update

--- add-municipio.py
+++ add-municipio.py
@@ -5,27 +5,30 @@ from tqdm import tqdm
 def carregar_municipios(conexao):
     consulta_municipios = "SELECT codigo, descricao FROM municipio"
     df_municipios = pd.read_sql_query(consulta_municipios, conexao)
+    df_municipios['codigo'] = df_municipios['codigo'].astype(str)
     return df_municipios.set_index('codigo')['descricao'].to_dict()
+
+def formatar_codigo_municipio(codigo):
+    # Garantir que o código tenha 4 dígitos, adicionando zeros à esquerda se necessário
+    return f"{codigo:0>4}"
 
 def atualizar_municipios_no_arquivo(arquivo, municipios):
     try:
         df = pd.read_csv(arquivo, low_memory=False)
+        df['municipio'] = df['municipio'].astype(str)
 
-        # Índice da coluna 'municipio'
-        coluna_municipio = 27
+        # Formatar os códigos dos municípios para garantir 4 dígitos
+        df['municipio_formatado'] = df['municipio'].apply(formatar_codigo_municipio)
 
-        # Verifica se a coluna 'municipio' e 'nome_municipio' existem
-        if 'municipio' in df.columns:
-            nome_municipio = df['municipio'].map(municipios).fillna(df['municipio'])
+        if 'municipio_formatado' in df.columns:
+            df['nome_municipio'] = df['municipio_formatado'].map(municipios)
+            df['nome_municipio'] = df['nome_municipio'].fillna('Não Encontrado')
 
-            if 'nome_municipio' in df.columns:
-                # Atualiza a coluna existente
-                df['nome_municipio'] = nome_municipio
-            else:
-                # Insere a nova coluna
-                df.insert(coluna_municipio + 1, 'nome_municipio', nome_municipio)
+            # Remover a coluna 'municipio_formatado' após o uso
+            df.drop('municipio_formatado', axis=1, inplace=True)
+
         else:
-            print(f"A coluna 'municipio' não foi encontrada em {arquivo}")
+            print(f"A coluna 'municipio_formatado' não foi encontrada em {arquivo}")
 
         df.to_csv(arquivo, index=False)
         print(f"Arquivo {arquivo} atualizado com sucesso.")
