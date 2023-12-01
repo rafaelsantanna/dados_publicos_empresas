cabd4a336e1cb4b52dc9480813cef7bc33cbb99c
Rafael Sant Anna <rafaeel.16@gmail.com>
2023-12-01 16:38:54 -0300

    funcionando

--- index.py
+++ index.py
@@ -1,55 +1,64 @@
 import sqlite3
 import pandas as pd
-from tqdm import tqdm
+from multiprocessing import Pool
 import time
 
-# Conectar ao banco de dados SQLite
-conexao = sqlite3.connect('cnpj.db')
+def process_chunk(chunk):
+    inicio = time.time()
 
-# Lista dos estados brasileiros
-# estados = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO']
-estados = ['DF']
+    # Operações vetorizadas para manipulação de strings
+    cnae_cols = chunk['cnae_fiscal_secundaria'].str.split(',', expand=True).iloc[:, :5]
+    cnae_cols.rename(columns=lambda x: f'cnae_fiscal_secundaria_{x+1}', inplace=True)
+    chunk = pd.concat([chunk, cnae_cols], axis=1)
 
-for estado in estados:
-    consulta = f"""
-    SELECT 
-      e.cnpj_basico,
-      e.razao_social,
-      e.natureza_juridica,
-      e.qualificacao_responsavel,
-      e.porte_empresa,
-      e.ente_federativo_responsavel, 
-      e.capital_social,
-      es.*,
-      c.descricao AS cnae_descricao
-    FROM 
-      empresas e
-    INNER JOIN 
-      estabelecimento es ON e.cnpj_basico = es.cnpj_basico
-    LEFT JOIN 
-      cnae c ON es.cnae_fiscal = c.codigo
-    WHERE
-      es.uf = '{estado}'"""
+    fim = time.time()
+    print(f"Chunk processado em {fim - inicio:.2f} segundos.")
+    return chunk
 
-    # Contar quantos registros temos para esse estado
-    total_registros = pd.read_sql_query(f"SELECT COUNT(*) FROM ({consulta})", conexao).iloc[0,0]
-    pbar = tqdm(total=total_registros, desc=f"Processando {estado}")
+def parallel_processing(consulta, conexao, chunksize, n_cores):
+    with Pool(n_cores) as pool:
+        chunks = pd.read_sql_query(consulta, conexao, chunksize=chunksize)
+        result = pool.map(process_chunk, chunks)
+        return pd.concat(result)
 
-    for chunk in pd.read_sql_query(consulta, conexao, chunksize=50000):  
-        inicio_chunk = time.time()
+def main():
+    conexao = sqlite3.connect('cnpj.db')
+    # estados = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO']
+    estados = ['SP', 'SE', 'TO']
 
-        # Dividir o campo 'cnae_fiscal_secundaria' e criar novas colunas
-        cnae_cols = chunk['cnae_fiscal_secundaria'].str.split(',', expand=True)
-        cnae_cols.rename(columns=lambda x: f'cnae_fiscal_secundaria_{x+1}', inplace=True)
-        
-        # Concatenar as novas colunas com o chunk original
-        chunk = pd.concat([chunk, cnae_cols], axis=1)
+    for estado in estados:
+        consulta = f"""
+        SELECT 
+          e.cnpj_basico,
+          e.razao_social,
+          e.natureza_juridica,
+          e.qualificacao_responsavel,
+          e.porte_empresa,
+          e.ente_federativo_responsavel, 
+          e.capital_social,
+          es.*,
+          c.descricao AS cnae_descricao,
+          m.descricao AS nome_municipio
+        FROM 
+          empresas e
+        INNER JOIN 
+          estabelecimento es ON e.cnpj_basico = es.cnpj_basico
+        LEFT JOIN 
+          cnae c ON es.cnae_fiscal = c.codigo
+        LEFT JOIN 
+          municipio m ON es.municipio = m.codigo
+        WHERE
+          es.uf = '{estado}'"""
 
-        chunk.to_csv(f'{estado}.csv', mode='a', index=False, header=not bool(chunk.index.start))
-        pbar.update(len(chunk))
-        fim_chunk = time.time()
-        print(f"Chunk processado em {fim_chunk - inicio_chunk:.2f} segundos.")
+        n_cores = 4  # Ajuste isso de acordo com a sua máquina
 
-    pbar.close()
+        print(f"Iniciando processamento para o estado {estado}.")
+        processed_data = parallel_processing(consulta, conexao, 10000, n_cores)
 
-conexao.close()
+        processed_data.to_csv(f'{estado}.csv', mode='a', index=False, header=True)
+        print(f"Processamento para o estado {estado} concluído. Dados salvos em {estado}.csv")
+
+    conexao.close()
+
+if __name__ == '__main__':
+    main()
